116b24d2405aba6c0142a7ec758cc577fa6dcebf
Ales Stibal <astib@mag0.net>
2017-03-19 22:36:24 +0100

    cli: add command: dns cache clear

--- cmdserver.cpp
+++ cmdserver.cpp
@@ -228,6 +228,17 @@ int cli_diag_dns_cache_stats(struct cli_def *cli, const char *command, char *arg
     return CLI_OK;
 }
 
+int cli_diag_dns_cache_clear(struct cli_def *cli, const char *command, char *argv[], int argc) {
+    inspect_dns_cache.lock();
+    
+    inspect_dns_cache.clear();
+    
+    cli_print(cli,"\nDNS cache cleared.");
+    inspect_dns_cache.unlock();
+    
+    return CLI_OK;
+}
+
 int cli_diag_dns_domain_cache_list(struct cli_def *cli, const char *command, char *argv[], int argc) {
     cli_print(cli, "\n Domain cache list:");
     std::string out;
@@ -625,6 +636,7 @@ void client_thread(int client_socket) {
                 diag_dns_cache = cli_register_command(cli, diag_dns, "cache", NULL, PRIVILEGE_UNPRIVILEGED, MODE_EXEC, "DNS traffic cache troubleshooting commands");
                         cli_register_command(cli, diag_dns_cache, "list", cli_diag_dns_cache_list, PRIVILEGE_PRIVILEGED, MODE_EXEC, "list all DNS traffic cache entries");
                         cli_register_command(cli, diag_dns_cache, "stats", cli_diag_dns_cache_stats, PRIVILEGE_PRIVILEGED, MODE_EXEC, "DNS traffic cache statistics");
+                        cli_register_command(cli, diag_dns_cache, "clear", cli_diag_dns_cache_clear, PRIVILEGE_PRIVILEGED, MODE_EXEC, "clear DNS traffic cache");
                 diag_dns_domains = cli_register_command(cli, diag_dns, "domain", NULL, PRIVILEGE_UNPRIVILEGED, MODE_EXEC, "DNS domain cache troubleshooting commands");
                         cli_register_command(cli, diag_dns_domains, "list", cli_diag_dns_domain_cache_list, PRIVILEGE_PRIVILEGED, MODE_EXEC, "DNS sub-domain list");
             diag_proxy = cli_register_command(cli, diag, "proxy",NULL, PRIVILEGE_PRIVILEGED, MODE_EXEC, "proxy related troubleshooting commands");
