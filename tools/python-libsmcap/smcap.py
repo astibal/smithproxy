80d45bd70c12ecc89f66df955b9c109c4c2f3d33
Ales Stibal <astib@mag0.net>
2019-02-09 20:32:41 +0100

    adding OpenSSL linking exception to license text

--- tools/python-libsmcap/smcap.py
+++ tools/python-libsmcap/smcap.py
@@ -13,7 +13,30 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Smithproxy.  If not, see <http://www.gnu.org/licenses/>.  """
+    along with Smithproxy.  If not, see <http://www.gnu.org/licenses/>.
+
+    Linking Smithproxy statically or dynamically with other modules is
+    making a combined work based on Smithproxy. Thus, the terms and
+    conditions of the GNU General Public License cover the whole combination.
+
+    In addition, as a special exception, the copyright holders of Smithproxy
+    give you permission to combine Smithproxy with free software programs
+    or libraries that are released under the GNU LGPL and with code
+    included in the standard release of OpenSSL under the OpenSSL's license
+    (or modified versions of such code, with unchanged license).
+    You may copy and distribute such a system following the terms
+    of the GNU GPL for Smithproxy and the licenses of the other code
+    concerned, provided that you include the source code of that other code
+    when and as the GNU GPL requires distribution of source code.
+
+    Note that people who make modified versions of Smithproxy are not
+    obligated to grant this special exception for their modified versions;
+    it is their choice whether to do so. The GNU General Public License
+    gives permission to release a modified version without this exception;
+    this exception also makes it possible to release a modified version
+    which carries forward this exception.
+
+    """
     
 
 import sys
