58b80cbee7e9da3b4dc0568b8242e0897f72266e
Ales Stibal <astib@mag0.net>
2020-01-26 11:54:41 +0100

    use cache for debug-localsrc if possible (argument for docker build are accepted as parameter)

--- tools/docker/scripts/make-run-own-debug-localsrc.sh
+++ tools/docker/scripts/make-run-own-debug-localsrc.sh
@@ -12,7 +12,7 @@ rm -rf $BUILD_DIR/sx/smithproxy/build
 rm -rf $BUILD_DIR/sx/smithproxy/venv
 
 sudo docker build ${BUILD_DIR}/sx -f ../../docker/0.9/run/ubuntu18.04-0.9-base.dockerfile --tag  astibal/smithproxy:ubuntu18.04-0.9-base
-sudo docker build --no-cache $1 ${BUILD_DIR}/sx -f ../../docker/0.9/run/ubuntu18.04-0.9-debug-localsrc.dockerfile --tag  astibal/smithproxy:ubuntu18.04-0.9-run-dbg-localsrc
+sudo docker build $1 ${BUILD_DIR}/sx -f ../../docker/0.9/run/ubuntu18.04-0.9-debug-localsrc.dockerfile --tag  astibal/smithproxy:ubuntu18.04-0.9-run-dbg-localsrc
 
 sudo ./runsx-debug.sh ubuntu18.04-0.9-run-dbg-localsrc
 sudo ./redir-output-chain.sh start
