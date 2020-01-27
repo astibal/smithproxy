a90bbc65fd046b58e3aac172674c9b8262f337dd
astib <astib@mag0.net>
2020-01-27 12:45:12 +0100

    docker: run smithproxy in isolated container (ie. when traffic is not to be tested)

---
+++ tools/docker/scripts/make-run-own-debug-localsrc-isolated.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# RUN THIS SCRIPT FROM ITS DIR 
+
+BUILD_DIR=`mktemp -d`
+mkdir $BUILD_DIR/sx
+
+cp -r ../../../../smithproxy/ $BUILD_DIR/sx
+
+rm -rf $BUILD_DIR/sx/smithproxy/cmake-*
+rm -rf $BUILD_DIR/sx/smithproxy/build
+rm -rf $BUILD_DIR/sx/smithproxy/venv
+
+sudo docker build ${BUILD_DIR}/sx -f ../../docker/0.9/run/ubuntu18.04-0.9-base.dockerfile --tag  astibal/smithproxy:ubuntu18.04-0.9-base
+sudo docker build $1 ${BUILD_DIR}/sx -f ../../docker/0.9/run/ubuntu18.04-0.9-debug-localsrc.dockerfile --tag  astibal/smithproxy:ubuntu18.04-0.9-run-dbg-localsrc
+
+sudo ./runsx-debug-isolated.sh ubuntu18.04-0.9-run-dbg-localsrc
+
+
+
