70fe58fc08ea45066dd3f334abdbff43db188900
astib <astib@mag0.net>
2020-04-18 23:43:22 +0200

    AsyncOCSP inherits from sobject
    - add a tracer - timestamp logging of events for troubleshooting

--- src/async/asyncocsp.hpp
+++ src/async/asyncocsp.hpp
@@ -10,49 +10,110 @@
 #include <async/asyncsocket.hpp>
 
 
+
+#define ASYNC_OCSP_DEBUG
+
+class Tracer {
+public:
+
+#ifdef ASYNC_OCSP_DEBUG
+    std::vector<std::pair<time_t,std::string>> trace;
+
+    void log(std::string const& s) {
+        trace.emplace_back(std::make_pair(::time(nullptr), s));
+    }
+#else
+    void log(std::string const& s) {}
+#endif
+};
+
+
+
 namespace inet::ocsp {
 
-        class AsyncOCSP : public AsyncSocket<int> {
-        public:
+    class AsyncOCSP : public AsyncSocket<int>, public socle::sobject {
+    public:
 
-            AsyncOCSP (X509 *cert, X509 *issuer, baseHostCX *cx, callback_t cb) :
-                    AsyncSocket(cx, std::move(cb)),
-                    query_(cert, issuer) {
-            };
+        AsyncOCSP (X509 *cert, X509 *issuer, baseHostCX *cx, callback_t cb) :
+                AsyncSocket(cx, std::move(cb)),
+                socle::sobject(),
+                query_(cert, issuer) {
+            log_tracer_("c-tor");
+        };
 
-            task_state_t update () override {
+        bool ask_destroy () override {
+            untap();
+            return true;
+        }
 
-                if (query_.run()) {
+        #ifdef ASYNC_OCSP_DEBUG
+        Tracer tracer;
+        #endif
 
-                    // reflect state to monitor socket
-                    if (query_.state() < inet::ocsp::OcspQuery::ST_REQ_SENT) {
-                        owner()->com()->set_write_monitor(socket());
-                    } else {
-                        owner()->com()->set_monitor(socket());
-                    }
+        std::string to_string (int verbosity = iINF) const override {
+            std::stringstream ss;
 
-                    return task_state_t::RUNNING;
+            ss << "AsyncOcsp [" << socket() << "] socket state: " << query().io().state_str();
+            ss << " OCSP state: " << state_str();
+
+            #ifdef ASYNC_OCSP_DEBUG
+            time_t now = time(nullptr);
+            for(auto e: tracer.trace) {
+                ss << std::endl << "[" << oid() << "]" << e.first - now << "s - " << e.second;
+            }
+            #endif
+            return ss.str();
+        }
+
+        void log_tracer_(const char* location) {
+            std::stringstream ss;
+            ss << location << ": state:" << query_.state_str() << " yield: " << query_.yield_str();
+            tracer.log(ss.str());
+
+        }
+
+        task_state_t update () override {
+
+
+            if (query_.run()) {
+
+                // reflect state to monitor socket
+                if (query_.state() < inet::ocsp::OcspQuery::ST_REQ_SENT) {
+                    owner()->com()->set_write_monitor(socket());
+                } else {
+                    owner()->com()->set_monitor(socket());
                 }
 
-                result_ = query_.yield();
-                result_state_ = query_.state();
-
-                return task_state_t::FINISHED;
+                log_tracer_("update");
+                return task_state_t::RUNNING;
             }
 
-            int &yield () override {
-                return result_;
-            }
+            result_ = query_.yield();
+            result_state_ = query_.state();
 
-            virtual void tap () {
-                AsyncSocket::tap(query_.io().socket());
-                query_.io().com_ = owner()->com();
-            }
+            log_tracer_("update");
+            return task_state_t::FINISHED;
+        }
 
-        private:
-            inet::ocsp::OcspQuery query_;
-            int result_ = -100;
-            int result_state_ = -100;
-        };
-    }
+        int &yield () override {
+            log_tracer_("yield");
+            return result_;
+        }
+
+        virtual void tap () {
+            log_tracer_("tap");
+            AsyncSocket::tap(query_.io().socket());
+            query_.io().com_ = owner()->com();
+        }
+
+        inet::ocsp::OcspQuery const& query() const { return query_; }
+    private:
+        inet::ocsp::OcspQuery query_;
+        int result_ = -100;
+        int result_state_ = -100;
+
+
+    DECLARE_C_NAME("AsyncOCSP");
+    };
+}
 #endif //ASYNCOCSP_HPP
