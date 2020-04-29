ab0f086ea75d2695bd61304b48a7165e7b1e37f7
astib <astib@mag0.net>
2020-04-29 22:41:41 +0200

    update dependencies script for ubuntu20.04

--- tools/ubuntu20.04-deps.sh
+++ tools/ubuntu20.04-deps.sh
@@ -3,7 +3,7 @@
 apt update && apt install -y \
 wget curl \
 python3 python3-pip python3-dev \
-libcli1.9 libconfig++9v5 libssl1.1 libunwind8 \
+libcli1.10 libconfig++9v5 libssl1.1 libunwind8 \
 libcli-dev libconfig-dev libconfig++-dev  libssl-dev libunwind-dev git g++-9 cmake make
 
 apt install -y iptables telnet iproute2 && \
@@ -16,6 +16,5 @@ ln -sf /usr/bin/gcc-9 /usr/bin/gcc && \
 ln -sf /usr/bin/gcc-9 /usr/bin/cc && \
 ln -sf /usr/bin/gcc-ar-9 /usr/bin/gcc-ar
 
-
-pip3 install pyroute2 pylibconfig2 m2crypto spyne zeep
+pip3 install pyroute2 pylibconfig2 m2crypto spyne zeep cryptography
 
