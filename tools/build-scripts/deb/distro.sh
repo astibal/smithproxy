f352e989aa27f7fea264ef22cbdd0413df8ade54
astib <astib@mag0.net>
2020-05-05 00:31:49 +0200

    adapt linux-deps.sh script improvements into separate distro.sh (needed somewhere else, where don't need inter-file dependency to avoid source duplication)

--- tools/build-scripts/deb/distro.sh
+++ tools/build-scripts/deb/distro.sh
@@ -18,33 +18,49 @@ elif [ "${OS}" = "AIX" ] ; then
 	OSSTR="${OS} `oslevel` (`oslevel -r`)"
 elif [ "${OS}" = "Linux" ] ; then
 	KERNEL=`uname -r`
-	if [ -f /etc/redhat-release ] ; then
+	if [ -f /etc/fedora-release ] ; then
+		DIST='Fedora'
+		PSEUDONAME=`cat /etc/fedora-release | sed s/.*\(// | sed s/\)//`
+
+		REV=`cat /etc/fedora-release | sed s/.*release\ // | sed s/\ .*//`
+	elif [ -f /etc/redhat-release ] ; then
 		DIST='RedHat'
-		PSUEDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
+		PSEUDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
 		REV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
+
 	elif [ -f /etc/SUSE-release ] ; then
 		DIST=`cat /etc/SUSE-release | tr "\n" ' '| sed s/VERSION.*//`
 		REV=`cat /etc/SUSE-release | tr "\n" ' ' | sed s/.*=\ //`
+
 	elif [ -f /etc/mandrake-release ] ; then
 		DIST='Mandrake'
-		PSUEDONAME=`cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//`
+		PSEUDONAME=`cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//`
 		REV=`cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//`
+
 	elif [ -f /etc/lsb-release ] ; then
 		eval `cat /etc/lsb-release`
 		DIST=$DISTRIB_ID
 		PSEUDONAME=$DISTRIB_CODENAME
 		REV=$DISTRIB_RELEASE
+
 	elif [ -f /etc/debian_version ] ; then
 		DIST="Debian"
 		REV="`cat /etc/debian_version | awk -F"/" '{ print $1 }' | awk -F"." '{ print $1 }'`"
-	fi
 
+		if [ "${REV}" = "bullseye" ]; then
+		    REV="11.0"
+		fi
 
-	if [ -f /etc/UnitedLinux-release ] ; then
+    elif [ -f /etc/alpine-release ] ; then
+        DIST="Alpine"
+        MAJ=`cat /etc/alpine-release | tr '_' ' ' | tr '.' ' ' | awk '{ print $1 }' `
+        MIN=`cat /etc/alpine-release | tr '_' ' ' | tr '.' ' ' | awk '{ print $2 }' `
+        REV="${MAJ}.${MIN}"
+
+	elif [ -f /etc/UnitedLinux-release ] ; then
 		DIST="${DIST}[`cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//`]"
 	fi
-	
-	OSSTR="${OS} ${DIST} ${REV}(${PSUEDONAME} ${KERNEL} ${MACH})"
+	OSSTR="${OS} ${DIST} ${REV}(${PSEUDONAME} ${KERNEL} ${MACH})"
 
 fi
 
