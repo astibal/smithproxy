9bfcb9259138fa65b120f557bc2c994146265dbb
astib <astib@mag0.net>
2021-01-10 20:20:45 +0100

    add some test coverage to addrobj.hpp
    Signed-off-by: astib <astib@mag0.net>

---
+++ src/policy/tests/addrobj_test.cpp
@@ -0,0 +1,26 @@
+#include <policy/addrobj.cpp>
+#include <log/logan.hpp>
+
+#include <gtest/gtest.h>
+
+TEST(CidrAddressTest, ZeroZeroMatchesAll) {
+    auto all = CidrAddress(cidr_from_str("0.0.0.0/0"));
+    auto ip4_min = cidr_from_str("1.0.0.1");
+    ASSERT_TRUE(all.contains(ip4_min) == 0);
+}
+
+
+TEST(CidrAddressTest, NonsenseInput) {
+    auto garbage = CidrAddress(cidr_from_str("this is not an address"));
+    ASSERT_TRUE(garbage.cidr() == nullptr);
+}
+
+TEST(CidrAddressTest, NonsenseInput2) {
+    auto garbage = CidrAddress(cidr_from_str("this.is.a.4"));
+    ASSERT_TRUE(garbage.cidr() == nullptr);
+}
+
+TEST(CidrAddressTest, NonsenseInput3) {
+    auto garbage = CidrAddress(cidr_from_str("this.is.a.4/23423"));
+    ASSERT_TRUE(garbage.cidr() == nullptr);
+}
