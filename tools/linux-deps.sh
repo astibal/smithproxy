055d9ff9bb789cfd2e4a8c0d46786e950fa65367
astib <astib@mag0.net>
2021-01-25 23:28:20 +0100

    remove spyne
    spyne is used in bend soap server
    
    Signed-off-by: astib <astib@mag0.net>

--- tools/linux-deps.sh
+++ tools/linux-deps.sh
@@ -2,7 +2,6 @@
 
 SX_LIBCONFIG_VER="9v5"
 SX_GCC_VER="8"
-SX_SPYNE_VER=">=2.13.2a0"
 
 
 OS=`uname -s`
@@ -104,7 +103,7 @@ if [ "${DIST}" = "Ubuntu" ]; then
 
     echo "... installing python libraries"
     pip3 install --upgrade pip
-    pip3 install pyroute2 pylibconfig2 m2crypto spyne${SX_SPYNE_VER} cryptography
+    pip3 install pyroute2 pylibconfig2 m2crypto cryptography
 
 elif [ "${DIST}" = "Debian" ]; then
 
@@ -157,10 +156,10 @@ elif [ "${DIST}" = "Debian" ]; then
     if [ "${DEB_MAJ}" = "11" ]; then
         apt install -y python3-m2crypto
         pip3 install posix-ipc
-        pip3 install pyroute2 pylibconfig2 spyne${SX_SPYNE_VER} cryptography
+        pip3 install pyroute2 pylibconfig2 cryptography
     else
         apt install -y python3-posix-ipc
-        pip3 install pyroute2 pylibconfig2 m2crypto spyne${SX_SPYNE_VER}  cryptography
+        pip3 install pyroute2 pylibconfig2 m2crypto cryptography
     fi
 
 elif [ "${DIST}" = "Alpine" ]; then
@@ -185,7 +184,7 @@ elif [ "${DIST}" = "Alpine" ]; then
     pip3 install --upgrade pip
     pip3 install wheel
     pip3 install posix-ipc
-    pip3 install pyroute2 pylibconfig2 m2crypto spyne${SX_SPYNE_VER}
+    pip3 install pyroute2 pylibconfig2 m2crypto
 
     LINK_TOOLCHAIN="N"
 
@@ -204,7 +203,7 @@ elif [ "${DIST}" = "Fedora" ]; then
     pip install --upgrade pip
     pip install wheel
     pip install posix-ipc
-    pip install pyroute2 pylibconfig2 m2crypto spyne${SX_SPYNE_VER} cryptography
+    pip install pyroute2 pylibconfig2 m2crypto cryptography
     LINK_TOOLCHAIN="N"
 
     cd ${OPW}
@@ -218,7 +217,7 @@ else
     echo "   "
     echo "and following packages to make smithproxy infrastructure work:"
     echo "   iptables telnet iproute2 python3 swig"
-    echo "   ... python3 packages: posix-ipc pyroute2 pylibconfig2 m2crypto spyne==2.13.2a0 cryptography"
+    echo "   ... python3 packages: posix-ipc pyroute2 pylibconfig2 m2crypto cryptography"
 
     exit 1;
 fi
