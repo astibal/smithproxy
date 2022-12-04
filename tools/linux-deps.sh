db0c916f0f32a396f255897e4d499620b1d28145
astib <astib@mag0.net>
2022-12-04 01:17:58 +0100

    add modern cmake to ubuntu 18.04 build

--- tools/linux-deps.sh
+++ tools/linux-deps.sh
@@ -96,7 +96,18 @@ if [ "${DIST}" = "Ubuntu" ]; then
         PIP="pip"
    fi
 
-    DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
+
+   # update toolchains to something reasonable
+   if [ "${REV}" = "18.04" ]; then
+        apt update && apt -y install gpg wget ca-certificates software-properties-common
+        wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
+        echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ bionic main' | tee /etc/apt/sources.list.d/kitware.list >/dev/null
+   fi
+
+
+
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get install -y tzdata
 
     echo "... installing essentials and libraries"
     apt update && apt install -y \
